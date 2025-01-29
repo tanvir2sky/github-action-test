fed1b31b886a5475bc3770a2a2f7b6d75f1f6a7e
Tanvir Hossain <tanvir2sky@gmail.com>
2025-01-29 15:39:47 +0600

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  rules: {
+    "no-var": "error",
+    semi: ["error", "always"],
+    "prefer-template": "error",
+  },
+};
